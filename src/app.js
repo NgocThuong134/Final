5c34019d7e8c1a0046bc47fc3e97b3300b569a76
Ngoc Thuong <166194276+NgocThuong134@users.noreply.github.com>
2024-05-31 13:07:17 +0700

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const nocache = require("nocache");
-const morgan = require('morgan');
-const sessionMiddleware = require('./middleware/user/sessionMiddleware');
+const morgan = require("morgan");
+const app = express();
+
+const sessionMiddleware = require("./middleware/user/sessionMiddleware");
 require("./db/mongodb");
 require("dotenv").config({ path: path.join(__dirname, ".env") });
-const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 const port = process.env.PORT || 8001;
 
@@ -23,23 +27,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 
 //set view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //load assets
-app.use(express.static(path.join(__dirname, "../public")))
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.use(express.static(path.join('uploads')))
+app.use(express.static(path.join("uploads")));
 
-app.use(express.static('js'));
+app.use(express.static("js"));
+app.get("/chat", (req, res) => {
+  res.render("chat");
+});
+app.get("/admin/chat", (req, res) => {
+  res.render("chat");
+});
+io.emit("hello", "world");
+io.on("connection", (socket) => {
+  socket.broadcast.emit("hi");
+  socket.emit('socket id', socket.id);
+});
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
 
 
+app.use("/", require("./routes/user-routes"));
+app.use("/", require("./routes/admin-routes"));
 
-
-//Load routers
-app.use('/', require('./routes/user-routes'))
-app.use('/', require('./routes/admin-routes'))
-//random string 
-
-app.listen(port, () => {
-    console.log(`Server Listening on port http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`Server Listening on port http://localhost:${port}`);
 });
